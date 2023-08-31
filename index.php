26cec90431554704e2b50617fd1655b3c0ef125b
wiboga <143158511+wiboga@users.noreply.github.com>
2023-08-30 23:02:32 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+
+<html>
+<head>
+<style>    
+table
+{
+border-collapse:collapse;
+}
+BODY
+{
+background-color:#666633;
+}
+a
+{
+color:white;
+text-decoration: none;
+}
+
+#menu td
+{
+width:13%;
+text-align:center;
+background-color:blueviolet;
+;
+color:white;
+font-size:20px;
+}
+#book td
+{
+width:20%;
+}
+</style>
+</head>
+<body>
+<!--Header table-->
+<table border="1" width="80%" align="center" height="20%">
+<tr>
+<td width="10%" bgcolor="#00CCCC">
+<img src="image/logo3.jpg" width="150px" height="150px" />
+</td>
+<td bgcolor="#000000" style="font-size:60px;color:green;font-variant:small-caps;font-family:arial;text-shadow:4px 4px 4px white" align="center">
+STUDENT MANAGEMENT SYSTEM
+</td>
+</tr>
+</table> 
+
+<!--Banner image table-->
+<table border="1" width="80%" align="center" height="40%">
+
+<table border="1" id="menu" width="80%" align="center" height="8%">
+<tr>
+<td><a href="index.php">HOME</a></td>
+<td><a href="reg.php">REGISTRATION</a></td>
+<td><a href="view.php">STUDENT RECORD</a></td>
+<td><a href="viewdel.php">EDIT</a></td>
+</tr>
+</table>
+
+<tr>
+<td>
+<img src="./image/must.jpg" width="100%" height="100%" />
+</td>
+</tr>
+</table> 
+
+<!--Menubar table-->
+ 
+
+<!--Heading table-->
+<table border="1" width="80%" align="center" height="8%">
+<tr bgcolor="#006666">
+<td style="font-size:20px;text-align:center;font-weight:bold;color:white;">
+<marquee>
+Made by Wiboga Wiboga
+</marquee>
+</td>
+</tr>
+</table> 
+
+
+</body>
+</html>
